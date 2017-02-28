e36943a2419619d5431e7764e6acdba2a89ad917
Mikhail <mikhail@iMac-Mikhail.local>
2017-02-28 13:02:01 +0700

    syntax fail fix

--- RVCalendarWeekView/Lib/MSWeekView.m
+++ RVCalendarWeekView/Lib/MSWeekView.m
@@ -205,7 +205,7 @@
     }
     for(int i = 0; i< self.daysToShow; i++){
         if(![_eventsBySection.allKeys containsObject:date.toDeviceTimezoneDateString]){
-            if date.toDeviceTimezoneDateString == nil { return }
+            if (date.toDeviceTimezoneDateString == nil) { return; }
             [_eventsBySection setObject:@[] forKey:date.toDeviceTimezoneDateString];
         }
         date = [date addDay];
